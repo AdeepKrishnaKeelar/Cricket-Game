a56fe8400c74f4efceed50e5cc971d8e78183f13
Adeep Krishna Keelar <eippeekeelar@gmail.com>
2022-05-08 14:37:34 +0530

    New additions, some more modifications will be done soon

--- CricketV2/Bits and Pieces/team_maker.cpp
+++ CricketV2/Bits and Pieces/team_maker.cpp
@@ -11,9 +11,29 @@
  #include<map>
 /* not using namespaces, screw the team! */
  int main(int argc, char* args[]) {
-     
-     std::fstream fin;
-     fin.open("Player_Names.txt",std::ios::in);
-     
-     return 0;
+    std::string line; 
+    /* ifstream: Stream class to read from files */
+    std::ifstream firstNames_file("Player_Names.txt");
+    std::ifstream lastNames_file("Players_Names_Last.txt");
+    /* try-catch exception block */
+    try {
+      /* if file fails to open, throw a value to the catch statement */
+      if(!firstNames_file.is_open() && !lastNames_file.is_open()) {
+        throw 1;
+      } else {
+        /* reading the contents of the names of the files. */
+        while(getline(firstNames_file,line)) {
+          std::cout<<line<<std::endl;
+        }
+        while(getline(lastNames_file,line)) {
+          std::cout<<line<<std::endl;
+        }
+        firstNames_file.close();
+        lastNames_file.close();
+      }
+    } catch(int n) {
+      /* when confirmed file failed to open! */
+      std::cout<<"Failed to open file!"<<std::endl;
+    }    
+    return 0;
  }
