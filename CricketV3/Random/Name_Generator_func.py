bfa9be92416382ed0c5acac7742c91c8abab7e7b
Adeep Krishna Keelar <eippeekeelar@gmail.com>
2022-05-27 10:11:59 +0530

    Modified, but not completely added

--- CricketV3/Random/Name_Generator_func.py
+++ CricketV3/Random/Name_Generator_func.py
@@ -11,18 +11,24 @@ import random #random library
 #end
 N=11 #11 Players in a Team
 #Read the names of the file
-file = open("Players_Names_First.txt","r")
-file2 = open("Players_Names_Last.txt","r")
+file_FirstName = open("Players_Names_First.txt","r")
+file_LastName = open("Players_Names_Last.txt","r")
 
-#Store the names in a default list
-firstnames = file.readlines()
-lastnames = file2.readlines()
+#Store the names in a default list and close existing files
+firstnames = file_FirstName.readlines()
+lastnames = file_LastName.readlines()
+file_FirstName.close()
+file_LastName.close()
 
 #The names are in a list, strip them of the '\n' 
 firstnames=[s.rstrip() for s in firstnames]
 lastnames=[s.rstrip() for s in lastnames]
 
-#Iterate 11 times for a combination using random
+#Open the main file to have the data written into
+file_writeNames = open("PlayersRandomGen.txt","w")
+
+#Iterate 11 times for a combination using random and close file
 while N != 0:
-    print(random.choice(firstnames)+' '+random.choice(lastnames))
+    file_writeNames.write(str(random.choice(firstnames)+' '+random.choice(lastnames)+'\n'))
     N -=1
+file_writeNames.close()
