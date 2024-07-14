1904de3fb1011b5bdca8fff7f491099782d1850a
AdeepKrishnaKeelar <eippeekeelar@gmail.com>
2024-07-14 14:18:31 +0530

    Started the CLI build for the game

--- dev/libs/commons.hpp
+++ dev/libs/commons.hpp
@@ -7,4 +7,41 @@
 #ifndef __COMMONS_INCLUDED__
 #define __COMMONS_INCLUDED__
 
+// Enumerated types that serve as a base for the CLI.
+typedef enum {
+    CREATE_CUSTOM_TEAM, // Command to create custom team. This command will have sub commands.
+    GAME_PLAY, // Command to play the game. This command will have sub commands.
+    EXIT // Command to exit.
+} Commands_Type;
+
+// Class for the input command buffer.
+typedef class Input_Command {
+    // Properties of the class.
+    private:
+        Commands_Type command_type;
+        std::string command_buffer; 
+    // Getters, Setters, etcetera, for Input_Command.
+    public:
+        
+} Command;
+
+// Intro function.
+void intro_func() {
+    const char* intro = "\
+██████  ██████  ██  ██████ ██   ██ ███████ ████████\n\
+██      ██   ██ ██ ██      ██  ██  ██         ██\n\
+██      ██████  ██ ██      █████   █████      ██\n\
+██      ██   ██ ██ ██      ██  ██  ██         ██\n\
+ ██████ ██   ██ ██  ██████ ██   ██ ███████    ██";
+
+    std::cout << intro << "\n";
+}
+
+
+// Setup the terminal for the program.
+void terminal_setup() {
+    std::cout<<"🏏 ";
+}
+
+
 #endif
--- dev/main.cpp
+++ dev/main.cpp
@@ -6,10 +6,29 @@
 
 // The HEADERS
 #include <iostream>
+#include <string>
 #include "libs/commons.hpp"
 
 
 int main(int argc, char* argv[]) {
-    std::cout<<"Hello";
+    // Defining the class to capture the user input.
+    Command cmd;
+    std::string input_command;
+    intro_func();
+    while(1) {
+        try {
+            terminal_setup();
+            std::getline(std::cin,input_command);
+            if(input_command.empty()) {
+                throw std::runtime_error(
+                    "Input Command is empty, there needs to be an entry."
+                );
+            }
+            
+        } catch(const std::exception& e) {
+            // Print the exception handed.
+            std::cout<<e.what()<<"\n";
+        }
+    }
     return EXIT_SUCCESS;
 }
